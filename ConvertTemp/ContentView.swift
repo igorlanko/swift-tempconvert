2c8b70cc559a9a2329365cd3744f698b26f7ffdb
igorlanko <mail@igorlanko.com>
2023-09-25 14:49:37 -0400

    adds layout. and math through switch

--- ConvertTemp/ContentView.swift
+++ ConvertTemp/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State private var convertFrom: String = "C"
+	@State private var convertTo: String = "K"
+	
+	@State private var initialValue: Double = 0
+	let units = ["C", "F", "K"]
+	
+	var result: Double {
+		var finalNumber: Double = 0
+		
+		switch(convertFrom, convertTo) {
+		case("C", "F"):
+			finalNumber = (initialValue * 9/5) + 32
+		case("C", "K"):
+			finalNumber = initialValue + 273.15
+		case("F", "C"):
+			finalNumber = (initialValue - 32) * 5/9
+		case("F", "K"):
+			finalNumber = (initialValue - 32) * 5/9 + 273.15
+		case("K", "C"):
+			finalNumber = initialValue - 273.15
+		case("K", "F"):
+			finalNumber = (initialValue - 273.15) * 9/5 + 32
+		default:
+			return 0
+		}
+		
+		return finalNumber
+	}
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		NavigationView {
+			Form {
+				Section {
+					Picker("Initial unit", selection: $convertFrom) {
+						ForEach(units, id: \.self) {
+							Text("\($0)º")
+						}
+					}
+					TextField("\(convertFrom)", value: $initialValue, format: .number)
+				} header: {
+					Text("Convert from")
+				}
+				Section {
+					Picker("Final unit", selection: $convertTo) {
+						ForEach(units, id: \.self) {
+							Text("\($0)º")
+						}
+					}
+					Text(result, format: .number)
+				} header: {
+					Text("Output")
+				}
+			}
+		}
     }
 }
 
