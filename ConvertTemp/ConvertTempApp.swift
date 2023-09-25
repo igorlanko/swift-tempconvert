14031ace846207229b4ee1a507094ce1413a8380
igorlanko <mail@igorlanko.com>
2023-09-25 12:39:47 -0400

    Initial Commit

---
+++ ConvertTemp/ConvertTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertTempApp.swift
+//  ConvertTemp
+//
+//  Created by stranger on 2023-09-25.
+//
+
+import SwiftUI
+
+@main
+struct ConvertTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
